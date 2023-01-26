d3363f547696cd973f4a48ee164622bc1655a8a4
schnitger <schnjona@kkg-do.eu>
2023-01-26 15:00:25 +0100

    ûpdate ui

--- src/Mine.java
+++ src/Mine.java
@@ -29,12 +29,12 @@ public class Mine{
        mine.minenTimer -= 0.1;
        if (explosionColision(player.x + player.width/2, player.y + player.height/2, 1000,500, 100) && minenTimer <= 0 && minenUeber < player.maxMinen){
            minenUeber++;
-           minenTimer = 4;
+           minenTimer = player.minenTime;
        }
 
         if (explosionColision(player2.x + player2.width/2, player2.y + player2.height/2, 1000,500, 100) && minenTimer <= 0 && mine.minenUeber < player2.maxMinen){
             mine.minenUeber++;
-            mine.minenTimer = player.minenTime;
+            mine.minenTimer = player2.minenTime;
         }
         if (explosionColision(player.x + player.width/2, player.y + player.height/2, 1000,500, 200)||explosionColision(player.x + player.width/2, player.y + player.height/2, 1745,245, 100)||explosionColision(player.x + player.width/2, player.y +player.height/2, 195,845, 100)){
             canPlase = false;
