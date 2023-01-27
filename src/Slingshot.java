8b71811b631fa8c8370008544ef16ca40ab3bcc4
kolhoff <schuelerx3@web.de>
2023-01-27 08:35:09 +0100

    Message

--- src/Slingshot.java
+++ src/Slingshot.java
@@ -65,32 +65,7 @@ public class Slingshot implements MouseListener, ActionListener {
             xLenght = (long) ((mousePos.getX()) - x1);
             playerRotation = Math.atan2(yLenght, xLenght);
         }
-        for (int i = allArrows.size(); i >= 1; i--) {
-            if (Objects.equals(allArrows.get(i - 1)[0], allArrows.get(i - 1)[2]) && allArrows.get(i - 1)[3] < allArrows.get(i - 1)[1]) {
-                allArrows.get(i - 1)[3] = 2500 + (allArrows.get(i - 1)[7] * allArrows.get(i - 1)[2] + allArrows.get(i - 1)[8]);
-            } else if (Objects.equals(allArrows.get(i - 1)[0], allArrows.get(i - 1)[2]) && allArrows.get(i - 1)[3] > allArrows.get(i - 1)[1]) {
-                allArrows.get(i - 1)[3] = 2500 - (allArrows.get(i - 1)[7] * allArrows.get(i - 1)[2] + allArrows.get(i - 1)[8]);
-            } else {
-                allArrows.get(i - 1)[3] = allArrows.get(i - 1)[7] * allArrows.get(i - 1)[2] + allArrows.get(i - 1)[8];
-            }
-        }
 
-        for (int i = allArrows.size(); i >= 1; i--) {
-            allArrows.get(i - 1)[5] = (allArrows.get(i - 1)[2]) - (allArrows.get(i - 1)[0]);//xLength
-            allArrows.get(i - 1)[6] = (allArrows.get(i - 1)[3]) - (allArrows.get(i - 1)[1]);//yLength
-            allArrows.get(i - 1)[4] = (Math.atan2(allArrows.get(i - 1)[6], allArrows.get(i - 1)[5]));//Rotation
-        }
-        for (int i = allArrows.size(); i >= 1; i--) {
-            if (allArrows.get(i - 1)[6] > -3 && allArrows.get(i - 1)[6] < 3 || allArrows.get(i - 1)[5] < 3 && allArrows.get(i - 1)[5] > -3) {
-                allArrows.remove(i - 1);
-            } else {
-                allArrows.get(i - 1)[0] = (allArrows.get(i - 1)[0] + (Math.cos(allArrows.get(i - 1)[4]) * 1500 * 0.01 * player.arrowVelosety));//xMovement
-                allArrows.get(i - 1)[1] = (allArrows.get(i - 1)[1] + (Math.sin(allArrows.get(i - 1)[4]) * 1500 * 0.01 * player.arrowVelosety));//yMovement
-            }
-        }
-
-        x1 = x1 + (int) (Math.cos(playerRotation) * 500 * 0.01);
-        y1 = y1 + (int) (Math.sin(playerRotation) * 500 * 0.01);
         for (int i = allArrows.size(); i >= 1; i--) {
             if (allArrows.get(i - 1)[0] >= panel.getWidth() || allArrows.get(i - 1)[0] <= 0) {
                 allArrows.remove(i - 1);
@@ -98,51 +73,34 @@ public class Slingshot implements MouseListener, ActionListener {
                 allArrows.remove(i - 1);
             }
         }
-
-
+        for (int i = allArrows.size(); i >= 1; i--) {
+            allArrows.get(i - 1)[0] = (allArrows.get(i - 1)[0] + allArrows.get(i - 1)[2] * 1500 * 0.01 * player.arrowVelosety);//xMovement
+            allArrows.get(i - 1)[1] = (allArrows.get(i - 1)[1] + allArrows.get(i - 1)[3] * 1500 * 0.01 * player.arrowVelosety);//yMovement
+        }
     }
 
     public void CreateArrow() {
-        if (mousePos != null) {
-            Double[] Arrow = new Double[9];
-            Arrow[0] = ((double) player.x);// x1 Index = 0
-            Arrow[1] = ((double) player.y);// y1 Index = 1
-            if (x1 > mousePos.getX()) {
-                Arrow[2] = ((mousePos.getX()) - 10000);//x2 Index = 2
-            } else if (x1 < mousePos.getX()) {
-                Arrow[2] = ((mousePos.getX()) + 10000);//x2 Index = 2
-            } else {
-                Arrow[2] = ((mousePos.getX()));//x2 Index = 2
-            }
-            Arrow[3] = ((mousePos.getY()));//y2 Index = 3
-            Arrow[4] = (0.0); //Rotation Index = 4
-            Arrow[5] = (0.0); //xLength Index = 5
-            Arrow[6] = (0.0); //yLength Index = 6
-            Arrow[7] = ((mousePos.getY() - y1) / (mousePos.getX() - x1));//LineareFunktion : m Index = 7
-            Arrow[8] = (y1 - ((mousePos.getY() - y1) / (mousePos.getX() - x1)) * x1); //LineareFunktion : b Index = 8
-            allArrows.add(Arrow);
-        }
+        Double[] Arrow = new Double[4];
+        Arrow[0] = ((double) player.x);// x1 Index = 0
+        Arrow[1] = ((double) player.y);// y1 Index = 1
+        Arrow[2] = Math.cos(playerRotation);//xVel
+        Arrow[3] = Math.sin(playerRotation);//yVel
+        allArrows.add(Arrow);
     }
 
     public void player2CreateArrow(double mouseX, double mouseY) {
-        Double[] Arrow = new Double[9];
-        Arrow[0] = ((double) player.x);// x1 Index = 0
-        Arrow[1] = ((double) player.y);// y1 Index = 1
-        if (x1 > mouseX) {
-            Arrow[2] = ((mouseX) - 500);//x2 Index = 2
-        } else if (x1 < mouseX) {
-            Arrow[2] = (mouseX + 500);//x2 Index = 2
-        } else {
-            Arrow[2] = (mouseX);//x2 Index = 2
-        }
 
-        Arrow[3] = (mouseY);//y2 Index = 3
-        Arrow[4] = (0.0); //Rotation Index = 4
-        Arrow[5] = (0.0); //xLength Index = 5
-        Arrow[6] = (0.0); //yLength Index = 6
-        Arrow[7] = ((mouseY - y1) / (mouseX - x1));//LineareFunktion : m Index = 7
-        Arrow[8] = (y1 - ((mouseY - y1) / (mouseX - x1)) * x1); //LineareFunktion : b Index = 8
-        this.allArrows.add(Arrow);
+        if (mousePos != null) {
+            long yLenght = (long) (mouseY - player.y);
+            long xLenght = (long) (mouseX - player.x);
+            double Rotation = Math.atan2(yLenght, xLenght);
+            Double[] Arrow = new Double[4];
+            Arrow[0] = ((double) player.x);// x1 Index = 0
+            Arrow[1] = ((double) player.y);// y1 Index = 1
+            Arrow[2] = Math.cos(Rotation);//xVel
+            Arrow[3] = Math.sin(Rotation);//yVel
+            allArrows.add(Arrow);
+        }
     }
 
     public void tick(){
@@ -163,7 +121,6 @@ public class Slingshot implements MouseListener, ActionListener {
             this.mousePos = mousePos;
         }
     }
-
     @Override
     public void mouseClicked(MouseEvent e) {}
 
@@ -173,7 +130,6 @@ public class Slingshot implements MouseListener, ActionListener {
             mouseDown = true;
         }
     }
-
     public boolean explosionColision(int x, int y, int x2, int y2, int distance) {
         return Math.abs(x - x2) < distance && Math.abs(y - y2) < distance;
     }
