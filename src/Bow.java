1fef633fd3f4eca7c6255d883d9c82dcc99e621b
schnitger <schnjona@kkg-do.eu>
2023-02-02 10:15:07 +0100

    level balance

--- src/Bow.java
+++ src/Bow.java
@@ -124,12 +124,12 @@ public class Bow implements MouseListener, ActionListener {
                 allArrows.add(Arrow);
             }
         }else {
-            long yLength = (long) ((player.y + player.height/2) - (panel.bow2.y1 + player.height / 2));
-            long xLength = (long) ((player.x + player.width/2) - (panel.bow2.x1 + player.width / 2));
+            long yLength = (long) ((panel.bow.y1 + player.height/2) - (y1 + player.height / 2));
+            long xLength = (long) ((panel.bow.x1 + player.width/2) - (x1 + player.width / 2));
             double playerRotation = Math.atan2(yLength, xLength);
             Double[] Arrow = new Double[5];
-            Arrow[0] = (double) panel.player2.x + player.width / 2;// x1 Index = 0
-            Arrow[1] = (double) panel.player2.y + player.height/2;// y1 Index = 1
+            Arrow[0] = (double) player.x + player.width / 2;// x1 Index = 0
+            Arrow[1] = (double) player.y + player.height/2;// y1 Index = 1
             Arrow[2] = Math.cos(playerRotation);//xVel
             Arrow[3] = Math.sin(playerRotation);//yVel
             Arrow[4] = playerRotation;
