ed08343addfa3902f447e0278a11870fdf057b8e
kolhoff <schuelerx3@web.de>
2023-02-02 08:39:27 +0100

    Boss Movement

--- src/Client.java
+++ src/Client.java
@@ -18,6 +18,7 @@ public class Client {
     boolean isClicked = false;
     boolean minePlaced = false;
     boolean exploded = false;
+
     public void setPanel(Panel panel) {
         this.panel = panel;
     }
@@ -27,46 +28,44 @@ public class Client {
         this.conFrame = conFrame;
 
         conFrame.setVisible(true);
-        frame = new GameFrame(this,coop);
+        frame = new GameFrame(this, coop);
         socket = new Socket(host, 30000);
         outputStream = new ObjectOutputStream(socket.getOutputStream());
         var socketThread = new Thread(() -> {
             while (true) {
                 try {
                     inputStream = new ObjectInputStream(socket.getInputStream());
-                    Object receivedObject = inputStream.readObject();
-                    if (receivedObject.getClass() == Message.class) {
-                        Message inMessage = (Message) receivedObject;
-                        panel.setPlayer2(inMessage.getX(), inMessage.getY(), inMessage.getName());
-                        panel.sword2.rotation = inMessage.getSwordRotation();
-                        panel.setRotation2(inMessage.rotation);
-                        panel.player2.setSwordEquipped(inMessage.swordPickedUp);
-                        if (inMessage.isClicked) {
-                            panel.bow2.player2CreateArrow(inMessage.mouseX, inMessage.mouseY);
-                        }
-                        panel.player2.setBowEquipped(inMessage.bowPickedUp);
-                        if (inMessage.minePlaced) {
-                            panel.mine2.createMine();
-                        }
-                        if (inMessage.exploded && panel.mine2.allMines.size() != 0) {
-                            panel.mine2.time = 1;
-                            panel.mine.explodeMines();
-                        }
-                        if (!inMessage.message.equals("")) {
-                            panel.messageInput.setText(inMessage.getName() + ": " + inMessage.message + "\n" + panel.messageInput.getText());
-                        }
-                        panel.ui.p2normHp = inMessage.normHp;
-                        panel.ui.death2 = inMessage.death;
-                        panel.player2.swordDamage = inMessage.swordDamage;
-                        panel.player2.bowDamage = inMessage.bowDamage;
-                        panel.player2.mineDamage = inMessage.mineDamage;
-                        panel.player2.mineTime = inMessage.mineTime;
-                        panel.player2.maxMines = inMessage.maxMines;
-                        panel.fist1.x2 = inMessage.fistX;
-                        panel.fist2.y2 = inMessage.fistY;
-                        panel.player2.fistEquipped = inMessage.fistEquipped;
-                        panel.player2.controled = inMessage.controled;
+                    Message receivedObject = (Message) inputStream.readObject();
+                    panel.setPlayer2(receivedObject.getX(), receivedObject.getY(), receivedObject.getName());
+                    panel.sword2.rotation = receivedObject.getSwordRotation();
+                    panel.setRotation2(receivedObject.rotation);
+                    panel.player2.setSwordEquipped(receivedObject.swordPickedUp);
+                    if (receivedObject.isClicked) {
+                        panel.bow2.player2CreateArrow(receivedObject.mouseX, receivedObject.mouseY);
                     }
+                    panel.player2.setBowEquipped(receivedObject.bowPickedUp);
+                    if (receivedObject.minePlaced) {
+                        panel.mine2.createMine();
+                    }
+                    if (receivedObject.exploded && panel.mine2.allMines.size() != 0) {
+                        panel.mine2.time = 1;
+                        panel.mine.explodeMines();
+                    }
+                    if (!receivedObject.message.equals("")) {
+                        panel.messageInput.setText(receivedObject.getName() + ": " + receivedObject.message + "\n" + panel.messageInput.getText());
+                    }
+                    panel.ui.p2normHp = receivedObject.normHp;
+                    panel.ui.death2 = receivedObject.death;
+                    panel.player2.swordDamage = receivedObject.swordDamage;
+                    panel.player2.bowDamage = receivedObject.bowDamage;
+                    panel.player2.mineDamage = receivedObject.mineDamage;
+                    panel.player2.mineTime = receivedObject.mineTime;
+                    panel.player2.maxMines = receivedObject.maxMines;
+                    panel.fist1.x2 = receivedObject.fistX;
+                    panel.fist2.y2 = receivedObject.fistY;
+                    panel.player2.fistEquipped = receivedObject.fistEquipped;
+                    panel.player2.controled = receivedObject.controled;
+
                 } catch (ClassNotFoundException e) {
                     System.out.println("Client hat Scheiße bekommen");
                 } catch (IOException e) {
@@ -78,13 +77,14 @@ public class Client {
         });
         socketThread.start();
     }
-    public void sendMessage(Player player,Sword sword) {
+
+    public void sendMessage(Player player, Sword sword) {
         try {
-            if(!panel.message.equals("")) {
+            if (!panel.message.equals("")) {
                 panel.messageInput.setText(name + ": " + panel.message + "\n" + panel.messageInput.getText());
             }
-            message = new Message(player.x, player.y,name, sword.rotation, bow.playerRotation,player.isBowEquipped(), bow.mousePos.getX(), bow.mousePos.getY(),isClicked, player.swordEquipped, minePlaced, exploded,panel.message, panel.ui.normHp, panel.ui.death, player.swordDamage + panel.inventory.getSwordDamage(), player.bowDamage + panel.inventory.getBowDamage(), player.mineDamage + panel.inventory.getMineDamage(), player.mineTime + panel.inventory.getMineTime(), player.maxMines + panel.inventory.getMaxMines(), panel.fist1.x, panel.fist1.y, panel.player1.fistEquipped, panel.sparned, panel.player1.controled);
-            if(!panel.serverDown) {
+            message = new Message(player.x, player.y, name, sword.rotation, bow.playerRotation, player.isBowEquipped(), bow.mousePos.getX(), bow.mousePos.getY(), isClicked, player.swordEquipped, minePlaced, exploded, panel.message, panel.ui.normHp, panel.ui.death, player.swordDamage + panel.inventory.getSwordDamage(), player.bowDamage + panel.inventory.getBowDamage(), player.mineDamage + panel.inventory.getMineDamage(), player.mineTime + panel.inventory.getMineTime(), player.maxMines + panel.inventory.getMaxMines(), panel.fist1.x, panel.fist1.y, panel.player1.fistEquipped, panel.sparned, panel.player1.controled);
+            if (!panel.serverDown) {
                 outputStream.writeObject(message);
             }
             isClicked = false;
@@ -94,7 +94,8 @@ public class Client {
             System.out.println(e.getMessage());
         }
     }
-    public void setClientPanel(Panel panel){
+
+    public void setClientPanel(Panel panel) {
         this.panel = panel;
     }
 
