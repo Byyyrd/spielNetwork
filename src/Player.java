78567a6d1870306748750f61050aa40da853a118
kolhoff <schuelerx3@web.de>
2023-02-03 08:31:11 +0100

    Boss Shot

--- src/Player.java
+++ src/Player.java
@@ -124,9 +124,11 @@ public class Player implements Serializable {
         }
         for (int i = 0; i < panel.boss.allFireballs.size(); i++){
             if (explosionCollision(x + width/2, y + height/2, (int)(panel.boss.allFireballs.get(i).get("X") + panel.fireballImage.getHeight(null)/8), (int)(panel.boss.allFireballs.get(i).get("Y") + panel.fireballImage.getHeight(null)/8), width/2 + panel.fireballImage.getHeight(null)/8)){
-                panel.ui.playerHitEnemy(panel.boss.damage);
-                panel.boss.allFireballs.remove(i);
-                controled = false;
+                if(!controled) {
+                    panel.ui.playerHitEnemy(panel.boss.damage);
+                    panel.boss.allFireballs.remove(i);
+                    controled = false;
+                }
             }
         }
         if (!panel.coop) {
